89fe47cd5bf2012822d673dd913db0072cfeff8b
Latina789 <115864165+Latina789@users.noreply.github.com>
2022-11-30 15:40:48 +0700

    ข้อ8

--- HW5/8.c
+++ HW5/8.c
@@ -1,50 +1,71 @@
 #include <stdio.h>
+#define MAXLEN 100
 typedef struct
 {
-    char id[100];
-    char name[100];
-    float score[100];
+    char id[MAXLEN];
+    char name[MAXLEN];
+    float score;
 }grade;
 int main()
 {
+    grade student[MAXLEN];
+    
     FILE *fptr;
     FILE *fptr1;
     fptr = fopen("studentScore.txt","r");
     fptr1 = fopen("studentGradeReport.txt","w");
     
-    int i = 0 ;
-    grade student;
     
-     while (fscanf(fptr, "%s %s %f",student.id[i],student.name[i],&student.score[i]) != EOF)
-     {
-        if (student.score[i] < 50 )
-        {
-            fprintf(fptr1, "Student ID: %s, Name: %s, Score %.2f grade: F",student.id[i],student.name[i],student.score[i]);
-            i++;
-        }
-        if (student.score[i] >= 50 && student.score[i] < 60)
-        {
-            fprintf(fptr1, "Student ID: %s, Name: %s, Score %.2f grade: D",student.id[i],student.name[i],student.score[i]);
-            i++;
-        }
-        if (student.score[i] >= 60 && student.score[i] < 70)
-        {
-            fprintf(fptr1, "Student ID: %s, Name: %s, Score %.2f grade: C",student.id[i],student.name[i],student.score[i]);
-            i++;
-        }if (student.score[i] >= 70 && student.score[i] < 80)
-        {
-            fprintf(fptr1, "Student ID: %s, Name: %s, Score %.2f grade: B",student.id[i],student.name[i],student.score[i]);
-            i++;
-        }
-        if (student.score[i] >= 50)
-        {
-            fprintf(fptr1, "Student ID: %s, Name: %s, Score %.2f grade: A",student.id[i],student.name[i],student.score[i]);
-            i++;
-        }
-        
-     }
+    int i, count = 0 ;
+    float min , max , mean ;
     
-
-
-
+    
+    while (fscanf(fptr,"%s %s %f",student[count].id,student[count].name,&(student[count].score)) != EOF)
+    {
+       /* printf("%s %s %.2f\n",student[count].id,student[count].name,student[count].score);
+        count++;*/
+        if (student[count].score < 50 )
+        {
+            fprintf(fptr1, "Student ID: %s, Name: %s,\tScore %.2f \tgrade: F\n",student[count].id,student[count].name,student[count].score);
+            count++;
+        }
+        else if (student[count].score >= 50 && student[count].score < 60)
+        {
+            fprintf(fptr1, "Student ID: %s, Name: %s,\tScore %.2f \tgrade: D\n",student[count].id,student[count].name,student[count].score);
+            count++;
+        }
+        else if (student[count].score >= 60 && student[count].score < 70)
+        {
+            fprintf(fptr1, "Student ID: %s, Name: %s,\tScore %.2f \tgrade: C\n",student[count].id,student[count].name,student[count].score);
+            count++;
+        }
+        else if (student[count].score >= 70 && student[count].score < 80)
+        {
+            fprintf(fptr1, "Student ID: %s, Name: %s,\tScore %.2f \tgrade: B\n",student[count].id,student[count].name,student[count].score);
+            count++;
+        }
+        else if (student[count].score >= 80)
+        {
+            fprintf(fptr1, "Student ID: %s, Name: %s,\tScore %.2f \tgrade: A\n",student[count].id,student[count].name,student[count].score);
+            count++;
+        }
+	}
+	
+    for(i = 0; i < count; i++)
+	{
+		mean = mean + student[i].score;
+		if(min > student[i].score || min == 0)
+		{
+			min = student[i].score;
+		}
+		if(max < student[i].score);
+		{
+			max = student[i].score;
+		}
+	}
+	mean = mean / count;
+	fprintf(fptr1,"Min score ; %.2f\n",min);
+	fprintf(fptr1,"Max score ; %.2f\n",max);
+	fprintf(fptr1,"Mean score ; %.2f\n",mean);
+    
 }
